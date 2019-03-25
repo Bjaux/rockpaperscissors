a073f2dd422103c6cc2bbb331ac9d750082d5050
Björn Kähler <kaehler@adesso-mobile.de>
2019-03-25 13:24:19 +0100

    develop: added comments

--- app/src/main/java/de/kaehler/rockpaperscissors/mvp/MvpBasePresenter.kt
+++ app/src/main/java/de/kaehler/rockpaperscissors/mvp/MvpBasePresenter.kt
@@ -1,9 +1,24 @@
 package de.kaehler.rockpaperscissors.mvp
 
 internal interface MvpBasePresenter<T : MvpBaseView>{
+    /**
+     * Bind a view to this presenter
+     *
+     * @param view the view to bind
+     */
     fun bind(view: T)
 
+    /**
+     * Unbind the view currently bound to this presenter
+     */
     fun unbind()
 
+    /**
+     * Unbind the passed view AND ONLY the passed view.
+     *
+     * Will be ignored, if the currently bound view is not instance equal (===)
+     *
+     * @param view the view to unbind
+     */
     fun unbind(view: T)
 }
